3317c1ec41e787cab775edd8751659e873aa6999
Gabriela Maximino <gabriela.maximino@hotmail.com>
2022-01-28 00:13:10 -0300

    modelos adicionados, paisagem construida

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ import * as THREE from './externo/three.module.js';
 import {OrbitControls} from "./externo/OrbitControls.js"
 import {Water} from './externo/Water.js'
 import {Sky} from './externo/Sky.js'
+import { GLTFLoader } from './externo/GLTFLoader.js';
 
 // Fonte: https://www.liquid.fish/current/threejs
 function SceneManager(canvas) {
@@ -14,6 +15,236 @@ function SceneManager(canvas) {
     const sun = buildSun();
     const water = buildWater();
     const orbitCon = setOrbitControls();
+    const model = buildModels();
+    
+    function buildModels() {
+        const loader = new GLTFLoader();
+
+        // garota em pe
+        loader.load( 'assets/modelos/garota-em-pe/scene.gltf', function ( gltf ) {
+            const girl= gltf.scene;
+            gltf.scene.scale.set(150, 150, 150)
+            girl.position.set(-300, 20, -600)
+            // girl.rotation.set(99, 0, 0)
+           
+            scene.add(girl);
+
+        }, undefined, function ( error ) {
+
+            console.error( error );
+
+        } )
+
+        // garota deitada
+        loader.load( 'assets/modelos/garota-deitada/scene.gltf', function ( gltf ) {
+            const girl= gltf.scene;
+            gltf.scene.scale.set(1.2, 1.2, 1.2)
+            girl.position.set(-250, 5, 100)
+            girl.rotation.set(99, 0, 0)
+           
+            scene.add(girl);
+
+        }, undefined, function ( error ) {
+
+            console.error( error );
+
+        } )
+        // bola volei
+        loader.load( 'assets/modelos/bola-volei/scene.gltf', function ( gltf ) {
+            const bola= gltf.scene;
+            gltf.scene.scale.set(0.75, 0.75, 0.75)
+            bola.position.set(-1700, 60, -1000)
+            // rede.rotation.set(0, -300, 0)
+           
+            scene.add(bola);
+
+        }, undefined, function ( error ) {
+
+            console.error( error );
+
+        } )
+
+        // rede volei
+        loader.load( 'assets/modelos/rede-volei/scene.gltf', function ( gltf ) {
+            const rede= gltf.scene;
+            gltf.scene.scale.set(1.6, 1.6, 1.6)
+            rede.position.set(-2000, 10, -1000)
+            rede.rotation.set(0, -300, 0)
+           
+            scene.add(rede);
+
+        }, undefined, function ( error ) {
+
+            console.error( error );
+
+        } )
+
+        // boia 
+        loader.load( 'assets/modelos/boia/scene.gltf', function ( gltf ) {
+            const boia= gltf.scene;
+            gltf.scene.scale.set(0.25, 0.25, 0.25)
+            boia.position.set(300, 0, 10)
+            // gaivota.rotation.set(0, -200, 0)
+           
+            scene.add(boia);
+
+        }, undefined, function ( error ) {
+
+            console.error( error );
+
+        } )
+        // mesa 
+        loader.load( 'assets/modelos/mesa/scene.gltf', function ( gltf ) {
+            const mesa= gltf.scene;
+            gltf.scene.scale.set(40, 40, 40)
+            mesa.position.set(300, 60, -800)
+            // gaivota.rotation.set(0, -200, 0)
+           
+            scene.add(mesa);
+
+        }, undefined, function ( error ) {
+
+            console.error( error );
+
+        } )
+        // gaivota
+        loader.load( 'assets/modelos/gaivota/scene.gltf', function ( gltf ) {
+            const gaivota= gltf.scene;
+            gltf.scene.scale.set(20, 20, 20)
+            gaivota.position.set(-700, 60, -700)
+            // gaivota.rotation.set(0, -200, 0)
+           
+            var gaivota2 = gaivota.clone();
+            gaivota2.position.set(-600, 60, -700)
+            gaivota2.rotation.set(0, 40, 0)
+
+
+            scene.add(gaivota);
+            scene.add(gaivota2);
+
+        }, undefined, function ( error ) {
+
+            console.error( error );
+
+        } )
+
+        // cachorro
+        loader.load( 'assets/modelos/cachorro/scene.gltf', function ( gltf ) {
+            const cachorro= gltf.scene;
+            gltf.scene.scale.set(100, 100, 100)
+            cachorro.position.set(-150, 0, -200)
+            cachorro.rotation.set(0, -200, 0)
+           
+
+            scene.add(cachorro);
+
+        }, undefined, function ( error ) {
+
+            console.error( error );
+
+        } )
+        // casa
+        loader.load( 'assets/modelos/casa/scene.gltf', function ( gltf ) {
+            const house= gltf.scene;
+            gltf.scene.scale.set(80, 80, 80)
+            house.position.set(0, 110, -2300)
+            house.rotation.set(0, 700, 0)
+           
+
+            scene.add(house);
+
+        }, undefined, function ( error ) {
+
+            console.error( error );
+
+        } )
+        // planta
+        loader.load( 'assets/modelos/planta/scene.gltf', function ( gltf ) {
+            var group = new THREE.Object3D();
+            const bush = gltf.scene;
+            gltf.scene.scale.set(3, 3, 3)
+
+            for (let i = 300; i < 5500; i+=300) {
+                var instance = bush.clone();
+                instance.position.set(i, 0, -3000);
+                group.add(instance);
+
+                var instance = bush.clone();
+                instance.position.set(-i, 0, -3000);
+                group.add(instance);
+
+            }
+
+            scene.add(group);
+
+        }, undefined, function ( error ) {
+
+            console.error( error );
+
+        } );
+
+        // coca
+        loader.load( 'assets/modelos/coca/Bottle_Only.gltf', function ( gltf ) {
+            const coke = gltf.scene;
+            gltf.scene.scale.set( 50, 50, 50 )
+            coke.position.set(-300, 30, -200)
+            scene.add( coke);
+
+        }, undefined, function ( error ) {
+
+            console.error( error );
+
+        } );
+
+        // 2 palmeiras
+        loader.load( 'assets/modelos/palmeira/scene.gltf', function ( gltf ) {
+            const light = new THREE.AmbientLight(0xFFFFFF, 0.4); // soft white light
+            scene.add(light);
+            var group = new THREE.Object3D();
+            var palmeira = gltf.scene;
+            palmeira.scale.set( 200, 200, 200 )
+            palmeira.position.set(-500, 10, -2300)
+
+            group.add(palmeira)
+            var instance = palmeira.clone();
+            instance.position.set(400, 10, -2300)
+
+            group.add(instance)
+            
+            scene.add(group)
+        }, undefined, function ( error ) {
+
+            console.error( error );
+
+        } );
+
+        // guarda sol
+        loader.load( 'assets/modelos/guarda-sol/scene.gltf', function ( gltf ) {
+            const guarda_sol = gltf.scene;
+            guarda_sol.scale.set( 2.5, 2.5, 2.5 )
+            guarda_sol.position.set(-460, 0, -200)
+
+            scene.add(guarda_sol)
+        }, undefined, function ( error ) {
+
+            console.error( error );
+
+        } );
+
+        // cadeira
+        loader.load( 'assets/modelos/cadeira/scene.gltf', function ( gltf ) {
+            const cadeira = gltf.scene;
+            cadeira.scale.set( 10, 10, 10 )
+            cadeira.position.set(-400, 0, -200)
+
+            scene.add(cadeira)
+        }, undefined, function ( error ) {
+
+            console.error( error );
+
+        } );
+        
+    }
 
     function buildScene() {
         const scene = new THREE.Scene();
@@ -21,16 +252,18 @@ function SceneManager(canvas) {
     }
 
     function buildRenderer(canvas) {
-        const renderer = new THREE.WebGLRenderer();
+        const renderer = new THREE.WebGLRenderer({alpha: false});
         renderer.setPixelRatio(window.devicePixelRatio);
         renderer.setSize(window.innerWidth, window.innerHeight);
         canvas.appendChild(renderer.domElement);
+        // renderer.gammaOutput = true;
+        // renderer.gammaFactor = -10;
         return renderer;
     }
 
     function buildCamera() {
         const camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 1, 20000);
-        camera.position.set(300, 200, -1000);
+        camera.position.set(0, 0, 1000);
         return camera;
     }
 
